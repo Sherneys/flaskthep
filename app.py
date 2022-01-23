0ea56a61c9a57a042f28e57f8839f41add1b32bd
Sherneys <viritphon.1234@gmail.com>
2022-01-23 19:55:17 +0700

    make it better

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = ["postgres://reuxpjnntrhhin:d25385a061597b190254ebcc134272c8778f53931510104095b94d490b1519ce@ec2-18-234-17-166.compute-1.amazonaws.com:5432/d7qvb8um38c7o"]
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('index.html', name=name)
 
 @app.route("/bank")
 def bank():
